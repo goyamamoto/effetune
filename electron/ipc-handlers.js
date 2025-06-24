5e30a046adcab21fbfd26240ccfdbacfe828d517
goyamamoto <goyammt@gmail.com>
2025-06-25 08:10:00 +0900

    Add HornResonatorPlus response tool to Electron

--- electron/ipc-handlers.js
+++ electron/ipc-handlers.js
@@ -496,6 +496,15 @@ function registerIpcHandlers() {
                   mainWin.loadFile('features/measurement/measurement.html');
                 }
               }
+            },
+            {
+              label: menuTemplate.settings.submenu[3].label, // HornResonatorPlus Response
+              click: () => {
+                const mainWin = constants.getMainWindow();
+                if (mainWin) {
+                  mainWin.loadFile('features/horn_resonator_plus_response.html');
+                }
+              }
             }
           ]
         },
@@ -911,6 +920,15 @@ function createMenu() {
             const mainWin = constants.getMainWindow();
             if (mainWin) {
               mainWin.loadFile('features/measurement/measurement.html');
+            }
+          }
+        },
+        {
+          label: 'HornResonatorPlus Frequency Response',
+          click: () => {
+            const mainWin = constants.getMainWindow();
+            if (mainWin) {
+              mainWin.loadFile('features/horn_resonator_plus_response.html');
             }
           }
         }
